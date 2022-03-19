a1cc0512c180edc4e5c5d38ad47e0320a6cc1758
Albert4940 <Albert-Mary.Dorce@uqtr.ca>
2022-03-19 03:48:24 -0400

    I added a bottom navigation menu

--- app/src/main/java/com/example/inf1030_tp1/activities/WelcomeActivity.java
+++ app/src/main/java/com/example/inf1030_tp1/activities/WelcomeActivity.java
@@ -3,13 +3,11 @@ package com.example.inf1030_tp1.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import com.example.inf1030_tp1.R;
-import com.example.inf1030_tp1.databinding.ActivityWelcomeBinding;
+import com.example.inf1030_tp1.frgagments.welcome.WelcomeFragment;
 
 public class WelcomeActivity extends AppCompatActivity {
     private ActivityWelcomeBinding binding;
@@ -19,14 +17,8 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_welcome);
         setContentView(binding.getRoot());
-
-        binding.btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(WelcomeActivity.this, LoginActivity.class);
-                startActivity(i);
-            }
-        });
+        getSupportFragmentManager().beginTransaction().
+                replace(R.id.fragment_container, new WelcomeFragment()).commit();
     }
 
 }
